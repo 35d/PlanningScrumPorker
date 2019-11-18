15fe4f375f918de1450fc057d558c9bca4b09327
maro-12 <naoya.kurahashi@gmail.com>
2019-11-18 20:23:32 +0900

    update expo and add react-navigation

--- App.tsx
+++ App.tsx
@@ -1,19 +1,71 @@
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react';
 import Home from './src/ui/containers/Home';
-import { NavigatorIOS } from 'react-native';
+import { View, Text } from 'react-native';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+
+class Hello extends React.Component {
+  static navigationOptions = {
+    title: 'Home',
+  };
+
+  didFocusSubscription = this.props.navigation.addListener(
+    'willBlur',
+    payload => {
+      console.log('@@@@@@@@@@@ willBlur');
+    },
+  );
+
+  render() {
+    return (
+      <View>
+        <Text>Heloooooooo</Text>
+        <Text
+          style={{ marginTop: 20, fontSize: 20, fontWeight: 'bold' }}
+          onPress={() => {
+            this.props.navigation.push('Detail');
+            this.didFocusSubscription.remove();
+          }}
+        >
+          to Detail
+        </Text>
+      </View>
+    );
+  }
+}
+
+class Detail extends React.Component {
+  render() {
+    return (
+      <View>
+        <Text>Detailllllllllll</Text>
+        <Text
+          style={{ marginTop: 20, fontSize: 20, fontWeight: 'bold' }}
+          onPress={() => this.props.navigation.goBack()}
+        >
+          Go Back
+        </Text>
+      </View>
+    );
+  }
+}
+
+const AppNavigator = createStackNavigator(
+  {
+    Home: Hello,
+    Detail: Detail,
+  },
+  {
+    initialRouteName: 'Home',
+    headerMode: 'none',
+  },
+);
+
+const AppContainer = createAppContainer(AppNavigator);
 
 const App = function app() {
-  return (
-    <NavigatorIOS
-      initialRoute={{
-        component: Home,
-        title: '',
-        navigationBarHidden: true,
-      }}
-      style={{ flex: 1 }}
-    />
-  );
+  return <AppContainer />;
 };
 
 export default App;
